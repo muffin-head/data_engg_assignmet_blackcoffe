8099b839cfb68ae0c9e0057938bb20221e49a22a
Tanmay Kadam <tanmay.kdm@gmail.com>
2022-11-17 06:14:25 +0530

    looping the excel file done

--- task1.py
+++ task1.py
@@ -1,7 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
+df = pd.read_excel('Input.xlsx', sheet_name='Sheet1')
 
-def scraper(url):
+
+def scraper(url,file_name):
     headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:55.0) Gecko/20100101 Firefox/55.0'}
     url = requests.get(url, headers=headers)
     url.status_code
@@ -11,10 +14,16 @@ def scraper(url):
     samples2 = soup.find_all(class_="td-post-content")
     for p in samples2:
         price = (p)
-        print(price.get_text().strip())
-        print()
+        a = (price.get_text().strip())
+        file1 = open(str(file_name),"w")
+  
+# \n is placed to indicate EOL (End of Line)
+        file1.writelines(a)
+        file1.close() 
 
 
+for i in (range(len(df))):
+  file_name=df['URL_ID'][i]
+  link  = df['URL'][i]
+  scraper(link,file_name)
 
-link  = "https://insights.blackcoffer.com/man-and-machines-together-machines-are-more-diligent-than-humans-blackcoffe/"
-scraper(link)
